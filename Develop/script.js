281da9c37205b686eccb2b34ccf3c75f6e09dde9
Levangul <150108077+Levangul@users.noreply.github.com>
2023-12-30 12:09:24 -0500

    cleaningcode

--- Develop/script.js
+++ Develop/script.js
@@ -7,25 +7,31 @@ const generateBtn = document.querySelector("#generate");
 //* const index = Math.floor(Math.random() * ....length);
 //* for (let i = 0; i < ....length; i++)
 //* kind of " if user choice += lowercase "
+    //* arrays to hold all numbers letters and symbols for the password
+    const upperCase = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+    const lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
+    const numbers = [ "1","2","3","4","5","6","7","8","9"]
+    const symbols = [ "!","@","#","$","%","^","&","*",] 
+    //* empty array to hold user choices after picking
+    let choiceCharacters = []
+    let characterLength = []
+    let givenPassword = []
+    let randomIndex = []
+    
 
-//* arrays to hold all numbers letters and symbols for the password
-  let upperCase = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
-  let lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
-  let numbers = [ "1","2","3","4","5","6","7","8","9"]
-  let symbols = [ "!","@","#","$","%","^","&","*",] 
-  //* empty array to hold user choices
-  let  choiceCharacters = []
+
+  
 
 
 //* creating function to generate password and prompts for user
 function generatePassword () {
 
-
-   let characterLength = window.prompt("Please choose how many characters you would like your password to contain? (please choose from 8 to 128)")
+  
+    characterLength = window.prompt("how many characters your password would you like to contain? (please choose from 8 to 128)")
 if (characterLength < 8 || characterLength > 128) {
-  return alert("Please choose from 8 to 128 characters")
+  alert("Please choose from 8 to 128 characters")
+  return generatePassword()
 } 
-
 
 if  (confirm("Do you want your password to include uppercase letters?"))  {
     choiceCharacters = choiceCharacters.concat(upperCase) 
@@ -47,14 +53,19 @@ if (confirm("Do you want your password to include symbols?") )  {
     
 }
 
-for (let i = 0; i < characterLength; i++);
- Math.floor(Math.random() * choiceCharacters.length);
+for (i = 0; i < characterLength.index; i++); {
+    randomIndex = Math.floor(Math.random() * choiceCharacters.length)
+    givenPassword += choiceCharacters[randomIndex]
+}
 
 
 
 
 
-return choiceCharacters;
+
+
+return givenPassword;
+
 
 
 
