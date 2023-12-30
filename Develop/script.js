f9cf93625737643f37463bbe10a1e277fb385ac5
Levangul <150108077+Levangul@users.noreply.github.com>
2023-12-29 22:16:21 -0500

    approach1

--- Develop/script.js
+++ Develop/script.js
@@ -6,29 +6,57 @@ const generateBtn = document.querySelector("#generate");
 //* confirm and window prompt
 //* const index = Math.floor(Math.random() * ....length);
 //* for (let i = 0; i < ....length; i++)
+//* kind of " if user choice += lowercase "
 
-const   userChoice = ""
-const   characterLength = 8;
-const   upperCase = [ 'abcdefghijklmopqrstuvwxyz' ]
-const   lowerCase = [ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ]
-const   numbers = [ '123456789']
-const   symbols = [ '!@#$%^&*()']
-
-
+//* arrays to hold all numbers letters and symbols for the password
+  let upperCase = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+  let lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
+  let numbers = [ "1","2","3","4","5","6","7","8","9"]
+  let symbols = [ "!","@","#","$","%","^","&","*",] 
+  //* empty array to hold user choices
+  let  choiceCharacters = []
 
 
 //* creating function to generate password and prompts for user
 function generatePassword () {
 
-const characterLength = window.prompt("How many characters would you like your password to contain? (please choose from 8 to 128)")
+
+   let characterLength = window.prompt("Please choose how many characters you would like your password to contain? (please choose from 8 to 128)")
 if (characterLength < 8 || characterLength > 128) {
-alert("Please choose from 8 to 128 characters")
+  return alert("Please choose from 8 to 128 characters")
+} 
+
+
+if  (confirm("Do you want your password to include uppercase letters?"))  {
+    choiceCharacters = choiceCharacters.concat(upperCase) 
+}
+    
+if (confirm("Do you want your password to include lowercase letters?") ) {
+    choiceCharacters = choiceCharacters.concat(lowerCase)
+    
+  
 }
 
+if (confirm("Do you want your password to include numbers? ") ) {
+    choiceCharacters = choiceCharacters.concat(numbers)
+    
+}
+
+if (confirm("Do you want your password to include symbols?") )  {
+    choiceCharacters = choiceCharacters.concat(symbols)
+    
+}
+
+for (let i = 0; i < characterLength; i++);
+ Math.floor(Math.random() * choiceCharacters.length);
 
 
 
-return ;
+
+
+return choiceCharacters;
+
+
 
 
 }
